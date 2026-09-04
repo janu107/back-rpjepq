f4cdf3f92f16d8cb0174f19ae0a9f9c5322f7831
jimmyn29325 <jimmyn29325@puertoquetzal.gob.gt>
2026-09-04 13:56:26 -0600

    chore: quitar script temporal de depuracion

--- dump2.js
+++
@@ -1,9 +0,0 @@
-require("dotenv").config();
-const mysql = require("mysql2/promise");
-(async () => {
-  const c = await mysql.createConnection({ host: process.env.DB_HOST, user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD, database: process.env.DB_NAME, port: Number(process.env.DB_PORT) });
-  const [r] = await c.query("SELECT ROUTINE_DEFINITION d FROM information_schema.routines WHERE routine_schema=DATABASE() AND routine_name=?", [process.argv[2]]);
-  console.log(r[0]?.d || "NO EXISTE");
-  await c.end();
-})();
